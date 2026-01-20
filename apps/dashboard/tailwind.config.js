6aaa9f0175c71d975f88f822d94180de1d310696
Tatiane Entler <tatiane.entler@gmail.com>
2026-01-20 11:36:50 -0300

    fix: adjusting the microfrontend integration to run once in the root directory

--- apps/dashboard/tailwind.config.js
+++ apps/dashboard/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "../../backoffice/src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "../../packages/**/*.{ts,tsx,css}",
   ],
   theme: {
     extend: {},
